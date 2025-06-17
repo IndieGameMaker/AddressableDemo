fee33bf7767862492c3af44d3851fe15e7f7764a
Lee JaeHyun <myleje@gmail.com>
2025-06-17 16:41:13 +0900

    ...

--- Assets/02_Scripts/Editor/LoadAssetManagerEditor.cs
+++ Assets/02_Scripts/Editor/LoadAssetManagerEditor.cs
@@ -1,7 +1,23 @@
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(LoadAssetManager))]
 public class LoadAssetManagerEditor : Editor
 {
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        LoadAssetManager manager = (LoadAssetManager)target;
 
+        if (GUILayout.Button("전사 로드"))
+        {
+            manager.LoadWarriorAsync();
+        }
+        
+        if (GUILayout.Button("전사 해제"))
+        {
+            manager.ReleaseWarrior();
+        }
+    }
 }
